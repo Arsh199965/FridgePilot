11a60d70f3866d9f3e781ee08a696276e4f90180
Arsh Ahmad <arsh199965@gmail.com>
2025-02-08 03:03:37 +0530

    commit

--- backend/app.py
+++ backend/app.py
@@ -8,6 +8,7 @@ import os
 from prediction import prediction_bp
 from recipe_prediction import recipe_bp
 from dotenv import load_dotenv
+from flask import Response
 
 load_dotenv()
 
@@ -20,6 +21,10 @@ app.config["CORS_HEADERS"] = "Content-Type"
 CORS(app, resources={r"/*": {"origins": FRONTEND}}, supports_credentials=True)
 
 init_db()
+@app.after_request
+def add_header(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
 
 app.register_blueprint(auth_bp, url_prefix="/auth")
 app.register_blueprint(pantry_bp, url_prefix="/pantry")
