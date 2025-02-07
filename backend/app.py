f3fd2549780d94abc676f0b52831bda1d8fd2a90
Arsh Ahmad <arsh199965@gmail.com>
2025-02-07 19:37:59 +0530

    New commit

--- backend/app.py
+++ backend/app.py
@@ -17,8 +17,7 @@ app.register_blueprint(pantry_bp, url_prefix="/pantry")
 app.register_blueprint(others_bp, url_prefix="/others")
 app.register_blueprint(prediction_bp, url_prefix="/prediction")
 app.register_blueprint(recipe_bp, url_prefix="/recipe")
-# def get_items():
-#     return jsonify({"message": "Items Received" ,"data":pantry_items}), 200
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
